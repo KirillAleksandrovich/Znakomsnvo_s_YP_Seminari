1c44b84bef8a0d69624f431c09203495445b0171
«Kirill» <kirill.feshev@bk.ru>
2022-09-07 21:39:35 +0300

    Task11

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -4,13 +4,30 @@
 // 78->8
 // 12->2
 // 85->8
+//88-> цифры одинаковые
 Console.Clear();
+//int num = new Random().Next(10, 100);
+// int firstDigit = num / 10; //7
+// int secondDigit = num % 10; //8
+
+//if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа{num} -> {firstDigit}");
+//else Console.WriteLine($"Наибольшая цифра числа {num} -> {secondDigit}");
+// if(firstDigit == secondDigit) Console.WriteLine($"Цифры равны");
+// else{
+// int max = firstDigit > secondDigit ? firstDigit : secondDigit;//Тернарный оператор. ?-тогда, :-иначе.
+// Console.WriteLine($"Наибольшая цифра числа {num} -> {max}");
+// }
 int num = new Random().Next(10, 100);
-int firstDigit = num / 10; //7
-int secondDigit = num % 10; //8
 
-if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа{num} -> {firstDigit}");
-else Console.WriteLine($"Наибольшая цифра числа {num} -> {secondDigit}");
+int MaxDigit(int number)
+{
+  int firstDigit = number / 10; //7
+  int secondDigit = number % 10; //8 
+  if(firstDigit == secondDigit) return 0;
+  return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
 
-int max = firstDigit > secondDigit ? firstDigit : secondDigit;//Тернарный оператор. ?-тогда, :-иначе.
-Console.WriteLine($"Наибольшая цифра числа {num} -> {max}");
+int maxDigit = MaxDigit(num);
+string result = maxDigit > 0 ? maxDigit.ToString() :"Цифры равны";
+Console.WriteLine($"Наибольшая цифра числа {num} -> {result}");
+
