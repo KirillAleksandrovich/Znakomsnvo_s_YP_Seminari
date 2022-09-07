3b6da8d39c3a4d01003b9984bb6c3a3e096f2148
«Kirill» <kirill.feshev@bk.ru>
2022-09-07 23:46:36 +0300

    Task16

---
+++ Task16/Program.cs
@@ -0,0 +1,21 @@
+﻿// 16. Напишите программу, которая принимает на 
+// вход два числа и проверяет, является ли одно 
+// число корнем другого.
+// 5, 25 -> да
+// -4, 16-> да
+// 25, 5 -> да
+// 8, 9 -> нет
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool square(int a1, int b1)
+{
+    return (a1 * a1 == b1 || b1 * b1 == a1);
+}
+bool result = square(num1, num2);
+if (result) Console.WriteLine($"Правда");
+else Console.WriteLine($"Ложь");
