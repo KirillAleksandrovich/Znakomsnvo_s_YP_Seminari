d5264ebd06dfdb9585b749bbcbf3d3a1d4635ff2
«Kirill» <kirill.feshev@bk.ru>
2022-09-14 10:56:37 +0300

    Task24,26,28,30

---
+++ Task28/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 28.
+// Напишите программу, 
+// которая принимает на вход сичло N
+// И выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 ->120
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int MultNumbers(int num)
+{
+    if(num != 0)
+    {
+    if(num < 0) num = -num;
+    int sum = 1;
+    for (int i = 1; i <= num; i++)
+        {
+            sum *= i; 
+        }
+        return sum;
+    } 
+    return 1;   
+}
+
+int multNumbers = MultNumbers(number);
+Console.WriteLine($"Факториал числа {number}! -> {multNumbers}");
---
+++ Task30/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 30.
+// Напишите программу, 
+// которая выводит массив из 8 элементов,
+// заполненный нулями и еденицами в случайном порядке.
+//  0 1 2 3 4 5 6 7
+// [1,0,1,1,0,1,0,0] 
+Console.Clear();
+int[] arr = new int[8];
+
+void FillArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        if (i == count -1) Console.Write($"{array[i]}]");
+        else Console.Write($"{array[i]}, "); 
+    }
+}
+
+FillArray(arr);
+PrintArray(arr);
