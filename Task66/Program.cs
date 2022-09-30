f0813332515ffad663442c9dd545f680f5c9c86f
«Kirill» <kirill.feshev@bk.ru>
2022-09-30 23:00:51 +0300

    Task64, 66, 68

---
+++ Task66/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 66. 
+// Задайте значения M и N. 
+// Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+Console.Clear();
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"\nM = {m}; N = {n} -> ");
+Console.WriteLine(NaturalNumbers(m, n));
+
+int NaturalNumbers(int m, int n)
+{
+    int numberM = m;
+    int numberN = n;
+    if (m > n)
+    {
+        numberM = n;
+        numberN = m;
+    }
+    return (numberN + numberM) * (numberN - numberM + 1) / 2;
+}
---
+++ Task68/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 68
+// Напишите программу 
+// вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.Clear();
+
+int m = EnterValue("Ввод m: ");
+int n = EnterValue("Ввод n: ");
+Console.WriteLine();
+int finalMethod = FunctionСalculations(m, n);
+Console.WriteLine($"m = {m}, n = {n} - > A(m,n) = {finalMethod} ");
+
+int EnterValue(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+int FunctionСalculations(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return FunctionСalculations(m - 1, 1);
+    }
+    else
+    {
+        return FunctionСalculations(m - 1, FunctionСalculations(m, n - 1));
+    }
+}
+Console.WriteLine();
